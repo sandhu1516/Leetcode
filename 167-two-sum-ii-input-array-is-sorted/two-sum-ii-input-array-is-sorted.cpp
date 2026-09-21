509c78d811487373e129bcccc2ce86f4ff95c8ec
Gurleen Kaur <gurleenksandhu15@gmail.com>
2026-09-21 12:49:57 +0530

    Time: 0 ms (100.00%) | Memory: 25.7 MB (5.40%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -3,38 +3,23 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int n = numbers.size();
 
-        for(int i=0;i<n;i++){
-              int required = target - numbers[i];
-              int start = i+1;
-              int end = n-1;
+        int left = 0;
+        int ryt = n-1;
 
-            while(start<=end){
-                int mid = start +(end-start)/2;
+        while(left<=ryt){
+            int sum = numbers[left] + numbers[ryt];
 
-                if(numbers[mid]==required){
-                    return {i+1,mid+1};
-                }
-                else if(numbers[mid]<required){
-                    start = mid+1;
-                }
-                else{
-                    end = mid-1;
-                }
+            if(sum == target){
+                return{left+1, ryt+1};
+            }
+            else if(sum < target){
+                left++;
+            }
+            else{
+                ryt--;
             }
         }
         return {};
     }
 };
 
-
-
-// BRUTE FORCE
-// int n = numbers.size();
-
-//         for(int i=0;i<n;i++){
-//             for(int j=i+1;j<n;j++){
-//                 if(numbers[i] + numbers[j] == target){
-//                     return {i+1 ,j+1};
-//                 }
-//             }
-//         }
